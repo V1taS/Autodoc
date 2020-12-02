3304bcce68f1b174c1b0e7f9f36cc54a66d7609a
Vitalii Sosin <375693@mail.ru>
2020-12-03 00:47:10 +0300

    Initial Commit

---
+++ Autodoc/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Autodoc
+//
+//  Created by Виталий Сосин on 03.12.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
